fecfdc3c598317d66194451060ac1444556a71d4
tanxiaolong <tanxiaolong@inke.cn>
2019-09-17 20:48:54 +0800

    chinese

--- test_chinese_characteristic.go
+++ test_chinese_characteristic.go
@@ -2,10 +2,13 @@ package main
 
 import "fmt"
 import "unicode"
+import "unicode/utf8"
 
 func main() {
 	fmt.Println("vim-go")
 	sentence := "合利屋可以取餐了hahaha"
+	l := utf8.RuneCountInString(sentence)
+	fmt.Println("length of sentence is:", l)
 	for _, word := range sentence {
 		if unicode.Is(unicode.Scripts["Han"], word) {
 			fmt.Println(word)
